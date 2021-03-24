33eac4a0d9b6b02cf6529768acf79317dc2a4330
lvdelrio <lvdelrio@miuandes.cl>
2021-03-24 16:44:39 -0300

    actualizacion #5

--- lab1.py
+++ lab1.py
@@ -5,96 +5,107 @@ Created on Mon Mar 22 13:15:11 2021
 @author: lucas
 """
 
-import scipy
-import numpy as np
+
 import random
 import sys
 
 def memorama():
     
-    imprimir()
-    print('Turno de Jugador 1')
+    screen()
+    print('player 1´s turn')
     
-    x = int(input('Ingresa un valor de izquierda a derecha como ve en pantalla: '))
+    x = int(input('Enter a value from left to right as you see on screen: '))
     if x >= len(matrix[0]):
         print('ERROR')
         memorama()
 
-    y = int(input('Ingresa un valor de arriba hasta abajo como indican los valores en la pantalla: '))
+    y = int(input('Enter a value from top to bottom as the values on the screen indicate: '))
     if y >= len(matrix):
         print('ERROR')
+        memorama()
+    elif matrix[y][x] == " ":
+        print("this coordinate has already been revealed, try another position")
         memorama()
     print(x,y,matrix[y][x],matrix2[y][x])
     matrix[y][x] = matrix2[y][x]
 
-    imprimir()
+    screen()
 #---------------------------------------------------------------------------|
     while True:
         
-        x2 = int(input('Ingresa un valor de izquierda a derecha como ve en pantalla: '))
-        y2 = int(input('Ingresa un valor de arriba hasta abajo como indican los valores en la pantalla: '))
+        x2 = int(input('Enter a value from left to right as you see on screen: '))
+        y2 = int(input('Enter a value from top to bottom as the values on the screen indicate: '))
         if (x2,y2) != (x,y):
             if x2 >= len(matrix[0]):
                 print('ERROR')
                 matrix[y][x] = '-'
-            if y2 >= len(matrix):
+            elif y2 >= len(matrix):
                 print('ERROR')
                 matrix[y][x] = '-'
-            break
+            elif matrix[y2][x2] == " ":
+                print("this coordinate has already been revealed, try another position")
+            else:
+                break
         else:
-            print("aweonao qlo te crei muy chistoso maricon hazlo bien")
+            print("ERROR, you placed the same coordinate, try again")
 #---------------------------------------------------------------------------|
 
     matrix[y2][x2] = matrix2[y2][x2]
-    imprimir()
-    turno=0
+    screen()
+    turn=0
     print(matrix)
-    condiciones(x,y,x2,y2,turno)
+    condition(x,y,x2,y2,turn)
 
 def memorama2():
-    imprimir()
-    print('Turno de Jugador 2')
+    screen()
+    print('player 2´s turn')
 
-    x = int(input('Ingresa un valor de izquierda a derecha como ve en pantalla: '))
+    x = int(input('Enter a value from left to right as you see on screen: '))
     if x >= len(matrix[0]):
         print('ERROR')
         memorama2()
 
-    y = int(input('Ingresa un valor de arriba hasta abajo como indican los valores en la pantalla: '))
+    y = int(input('Enter a value from top to bottom as the values on the screen indicate: '))
     if y >= len(matrix):
         print('ERROR')
         memorama2()
+    elif matrix[y][x] == " ":
+        print("this coordinate has already been revealed, try another position")
+        memorama2()
 
     matrix[y][x] = matrix2[y][x]
-    imprimir()
+    screen()
 #--------------------------------------------------------------------------|
     while True:
         
-        x2 = int(input('Ingresa un valor de izquierda a derecha como ve en pantalla: '))
-        y2 = int(input('Ingresa un valor de arriba hasta abajo como indican los valores en la pantalla: '))
+        x2 = int(input('Enter a value from left to right as you see on screen: '))
+        y2 = int(input('Enter a value from top to bottom as the values on the screen indicate: '))
         if (x2,y2) != (x,y):
             if x2 >= len(matrix[0]):
                 print('ERROR')
                 matrix[y][x] = '-'
-            if y2 >= len(matrix):
+            elif y2 >= len(matrix):
                 print('ERROR')
                 matrix[y][x] = '-'
-            break
+            elif matrix[y2][x2] == " ":
+                print("this coordinate has already been revealed, try another position")
+            else:
+                break
         else:
-            print("aweonao qlo te crei muy chistoso maricon hazlo bien")
+            print("ERROR, you placed the same coordinate, try again")
 #---------------------------------------------------------------------------|
     matrix[y2][x2] = matrix2[y2][x2]
     
-    imprimir()
-    turno=1
+    screen()
+    turn=1
     print(matrix)
-    condiciones(x,y,x2,y2,turno)
+    condition(x,y,x2,y2,turn)
 
-def condiciones(x,y,x2,y2,turno):
+def condition(x,y,x2,y2,turn):
     global p1
     global p2
-#----------------------------------------------------turnos-------------------|
-    if turno == 0:
+#----------------------------------------------------turns-------------------|
+    if turn == 0:
         if matrix[y][x] != matrix[y2][x2]:
             matrix[y2][x2] = "-"
             matrix[y][x] = "-"
@@ -103,11 +114,9 @@ def condiciones(x,y,x2,y2,turno):
             matrix[y2][x2] = " "
             matrix[y][x] = " "
             p1 += 1 
-            print("player 1 ganaste un punto ahora tienes ",p1)
-            fin_del_juego(x,y,x2,y2,turno)
-            print("te vuelve a tocar")
-            memorama()
-    elif turno == 1:
+            print("player 1 won a point, now you have: ",p1)
+            endgame(x,y,x2,y2,turn)
+    elif turn == 1:
         if matrix[y][x] != matrix[y2][x2]:
             matrix[y2][x2] = "-"
             matrix[y][x] = "-"
@@ -116,12 +125,12 @@ def condiciones(x,y,x2,y2,turno):
             matrix[y2][x2] = " "
             matrix[y][x] = " "
             p2 += 1 
-            print("player 2 ganaste un punto ahora tienes ",p2)
-            fin_del_juego(x,y,x2,y2,turno)
+            print("player 2 won a point, now you have: ",p2)
+            endgame(x,y,x2,y2,turn)
 
     
-#-----------------------------------------------------------------------------|
-def recorre():
+#------------------------------------check endgame-----------------------------------------|
+def check():
     for i in range(len(matrix)):
         for j in range(len (matrix[0])):
             if matrix[i][j] != " ":
@@ -129,33 +138,33 @@ def recorre():
     return True
 
 
-def fin_del_juego(x,y,x2,y2,turno):
+def endgame(x,y,x2,y2,turn):
 
-    equal = recorre()
+    equal = check()
     
     if equal == True:
-        print('El jugador tiene',p1,'pares')
-        print('El jugador tiene',p2,'pares')
+        print('the player 1 have ',p1,'pairs')
+        print('the player 2 have ',p2,'pairs')
         if p1 > p2:
-            print('El ganador fue el jugador 1')
+            print('The winner was player 1')
         elif p2 > p1:
-            print('El ganador fue el jugador 2')
+            print('The winner was player 2')
         elif p2 == p1:
-            print('Fue un empate!')
+            print('it was a tie!')
         elif p1 == p2:
-            print('Fue un empate!')
+            print('it was a tie!')
         sys.exit() 
     else:
-        if turno == 0:
-            print("te vuelve a tocar")
+        if turn == 0:
+            print("it's your turn again")
             memorama()
         else:
-            print("te vuelve a tocar")
+            print("it's your turn again")
             memorama2()
 
 
 
-def imprimir():
+def screen():
     contador = 0
     for i in range(len(matrix[0])):
         print("",i,"", end = '')
@@ -171,20 +180,20 @@ def imprimir():
 def main():
     memorama()
         
-#----------------------------------------variables globales----------------------------------|       
+#----------------------------------------global variables----------------------------------|       
 x=0        
 y=0
 x2=0
 y2=0
 p1 = 0
 p2 = 0
-turno=0
+turn=0
 equal=0
 lista=[]
 matrix = []
 matrix2 = [[],[]]
 
-cant_cartas=int(input("cantidad de cartas individuales? luego yo agregare su par y las revolvere"))
+cant_cartas=int(input("enter the number of the pair you want to play"))
 
 
 for i in range(cant_cartas):
